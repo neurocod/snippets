0f211fd0edd04c26647c13a6b00a2a414c8a6a7a
neurocod <neurocod@gmail.com>
2019-10-04 14:29:31 +0300

    Comments inside aliases.bat

--- windows/aliases.bat
+++ windows/aliases.bat
@@ -1,3 +1,3 @@
+REM open this folder in Explorer to edit aliases https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
 start %~dp0
 echo off
-REM https://stackoverflow.com/questions/17063947/get-current-batchfile-directory

5df6d32bd0a6ee9063fb5e90dd602c2ed1758e47
neurocod <neurocod@gmail.com>
2019-10-05 17:34:57 +0300

    n.bat -> r.bar +echo off

--- windows/r.bat
+++ windows/r.bat
@@ -1,6 +1,7 @@
+echo off
 REM This script launches lf terminal file manager https://github.com/gokcehan/lf
-REM and then cd to last directory, open on lf.
-REM Name `n` is to mimic NNN file manager https://github.com/jarun/nnn or ranger file manager.
+REM and then cd to last directory, open in lf.
+REM Name `r` is to mimic ranger file manager, which I use on linux
 lf -last-dir-path=%tmp%\lfLastDir.txt
 set /p lastDir=<%tmp%\lfLastDir.txt
 cd %lastDir%

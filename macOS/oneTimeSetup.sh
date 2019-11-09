8b97cd79bd49f3e3def551d6a0618e74b32d29cb
neurocod <neurocod@gmail.com>
2019-11-09 14:57:39 +0200

    oneTimeSetup += ranger settings

--- macOS/oneTimeSetup.sh
+++ macOS/oneTimeSetup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #one-time setup:
 brew install ranger
-ranger --copy-config=all #creates config; set show_hidden_files true there
+ranger --copy-config=all#creates config
+#set show_hidden_files true;
+#set preview_script ~/.config/ranger/scope.sh
+#set use_preview_script true
+#set draw_borders separators
 brew install mc
 brew install nano
 brew install coreutils
